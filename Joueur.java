7cad4700d01acb769ba98575e2201f183dd83434
NathanCarel <51905301+NathanCarel@users.noreply.github.com>
2019-06-18 08:08:30 +0200

    Add files via upload

--- Joueur.java
+++ Joueur.java
@@ -1,30 +1,27 @@
 public class Joueur
 {
-	private static int    numJoueur = 1;
+    private static String[] tabCouleurs = new String[] {"Rouge", "Jaune", "Vert", "Bleu", "Violet", "Rose"};
+    private static int nbJoueurs;
 
-	private String   nom;
-	private int      nbPoint;
-	private Robot    robot1;
-	private Robot    robot2;
-	private Action[] ensAction;
+    private Robot robot1;
+    private Robot robot2;
+    private int   nbPoints;
+    private Base  base;
+    private Ordre[] stockOrdre;
 
-	
+    public Joueur()
+    {
+        this.robot1 = new Robot("Robot",,,tabCouleurs[Joueur.nbJoueurs]);
+        this.robot2 = new Robot("Robot",,,tabCouleurs[Joueur.nbJoueurs]);
+        this.base   = new Base ("Base" ,,,tabCouleurs[Joueur.nbJoueurs]);
+        this.nbPoints = 0;
+        this.stockOrdre = new Ordre[8];
+        Joueur.nbJoueurs++;
+    }
 
-	public Joueur(int numJoueur)
-	{
-		numJoueur = Controleur.numJoueur ++;
-		Controleur.numJoueur = numJoueur; 
-
-		this.nbPoint = 0;
-		this.robot1  = new Robot();
-		this.robot2  = new Robot();
-
-	}
-
-	public void ajouterCristaux()
-	{
-		this.nbPoint++;
-	}
-
-
+    public Robot getRobot1()     { return this.robot1;        }
+    public Robot getRobot2()     { return this.robot2;        }
+    public int   getNbPoints()   { return this.nbPoints;      }
+    public Base  getBase()       { return this.base;          }
+    public Ordre getOrdre(int i) { return this.stockOrdre[i]; }
 }
