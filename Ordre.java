f4caf6e918ab78c87d7f8a03a753faf8232f0d40
NathanCarel <51905301+NathanCarel@users.noreply.github.com>
2019-06-18 08:11:14 +0200

    Add files via upload

---
+++ Ordre.java
@@ -0,0 +1,11 @@
+public class Ordre
+{
+    private int nbExemplaires;
+    private String type;
+
+    public Ordre(int nbExemplaires, String type)
+    {
+        this.nbExemplaires = nbExemplaires;
+        this.type          = type;
+    }
+}
